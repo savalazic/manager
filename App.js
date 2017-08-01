017ab6c8ee661c47faf325db4e2f0871f7c4c578
savalazic <savalazic9@gmail.com>
2017-08-01 13:31:18 +0200

    chore: initialize firebase

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
 
-export default class App extends React.Component {
+import reducers from './reducers';
+
+export default class App extends Component {
+  
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyBGjuwUeDmXBKSurotEf52dukjEGmKegbk',
+      authDomain: 'managerapp-2b4f8.firebaseapp.com',
+      databaseURL: 'https://managerapp-2b4f8.firebaseio.com',
+      projectId: 'managerapp-2b4f8',
+      storageBucket: 'managerapp-2b4f8.appspot.com',
+      messagingSenderId: '29437786813'
+    };
+    firebase.initializeApp(config);
+  }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes youadsadsads reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={styles.container}>
+          <Text>WELCOME</Text>
+        </View>
+      </Provider>
     );
   }
 }
